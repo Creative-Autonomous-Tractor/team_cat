85b99f3277eecd6c5b2d46ae475325df255e4cfa
jihoon416 <63445490+jihoon416@users.noreply.github.com>
2020-10-21 18:29:39 +0900

    Limitless & Lower Curve coeff.
    Makes slower on curve

--- src/ourdrive_node.cpp
+++ src/ourdrive_node.cpp
@@ -20,14 +20,14 @@ const double L = 0.7;
 // const double k = 3.5; //jaewon's algorithm sqrt
 const double k = 7.5; //jaewon's algorithm cubed
 const double deltath = 0.1; // disparity gijoon
-const double MAX_Velocity = 15.0; //
+const double MAX_Velocity = 100;//limited and crahsed so trying limitless again 15.0; //
 const double MAX_STEERING_ANGLE = 0.4819;// 25 * 3.14 / 180; //
 const double MIN_SAFE_Distance = 15.0; //
 const double additional_gap = 5; //originally 1.2
 const int average_size = 9;
 
 const double DELTA_CURVE_ANGLE_THRESHOLD = 5; //in degrees
-const double CURVE_VELOCITY_COEFFICIENT = 0.2; //how much lower the speed will be when curving
+const double CURVE_VELOCITY_COEFFICIENT = 0.1; //how much lower the speed will be when curving
 
 double min(double a, double b) {
     return a > b ? b : a;
