0bb5feb18e93bb899001fcc7baa32a7953f606f8
jihoon416 <63445490+jihoon416@users.noreply.github.com>
2020-10-23 02:17:18 +0900

    Wow forgot start_velocity again
    Co-Authored-By: jaewonlee16 <73290953+jaewonlee16@users.noreply.github.com>

--- src/ourdrive_node.cpp
+++ src/ourdrive_node.cpp
@@ -38,7 +38,7 @@ int start_velocity = 100;
 bool is_max_speed_okay = true;
 bool is_max_speed_okay_after = false;
 
-const double jilju_angle = 0.1 * 3.14 / 180;
+const double jilju_angle = 0.2 * 3.14 / 180;
 
 double min(double a, double b) {
     return a > b ? b : a;
@@ -248,20 +248,20 @@ else if (scan_number > scan_number_gijoon){*/
             is_max_speed_okay = false;
             start_velocity = 30;
         }
-        // if (velocity > 1) start_velocity -= 0.01;
+        // if (start_velocity > 1) start_velocity -= 0.01;
         // else if (scan_msg->ranges[scan_msg->ranges.size() / 2] < 15) velocity /= 5; //velocity is 20
         // else if (scan_msg->ranges[scan_msg->ranges.size() / 2] < 20) velocity /= 2; //velocity is 50
-        else if (scan_msg->ranges[scan_msg->ranges.size() / 2] < 10 && velocity > 1) velocity -= 0.1;
+        else if (scan_msg->ranges[scan_msg->ranges.size() / 2] < 10 && start_velocity > 1) start_velocity -= 0.1;
     }
     if (is_max_speed_okay_after) {
 		int max_index = maximum_element_index(filtered_ranges);
 		steering_angle = scan_msg->angle_min + scan_msg->angle_increment * (truncated_start_index_ + max_index);
 		steering_angle = std::clamp(steering_angle, -jilju_angle, jilju_angle);
 	    if (scan_msg->ranges[scan_msg->ranges.size()/2] < 8) is_max_speed_okay_after = false;
-        // if (velocity > 1) start_velocity -= 0.01;
+        // if (start_velocity > 1) start_velocity -= 0.01;
         // else if (scan_msg->ranges[scan_msg->ranges.size() / 2] < 15) velocity /= 5;
         // else if (scan_msg->ranges[scan_msg->ranges.size() / 2] < 20) velocity /= 2;
-        else if (scan_msg->ranges[scan_msg->ranges.size() / 2] < 10 && velocity > 1) velocity -= 0.1;
+        else if (scan_msg->ranges[scan_msg->ranges.size() / 2] < 10 && start_velocity > 1) start_velocity -= 0.1;
 	}
     else {
 	if (scan_msg->ranges[scan_msg->ranges.size()/2] > 50) {
