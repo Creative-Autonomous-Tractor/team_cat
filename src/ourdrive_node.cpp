48e5c18e7cd8d2927204324169c2dfc12ace76e1
S. J. Hoon <63445490+jihoon416@users.noreply.github.com>
2020-10-26 01:56:56 +0900

    Activate Jilju again

--- src/ourdrive_node.cpp
+++ src/ourdrive_node.cpp
@@ -269,13 +269,13 @@ else if (scan_number > scan_number_gijoon){*/
         // if (start_velocity > 1) start_velocity -= 0.01;
         // else if (scan_msg->ranges[scan_msg->ranges.size() / 2] < 15) velocity /= 5;
         // else if (scan_msg->ranges[scan_msg->ranges.size() / 2] < 20) velocity /= 2;
-        else if (scan_msg->ranges[scan_msg->ranges.size() / 2] < 20 && start_velocity > 1) start_velocity -= 0.1;
+        else if (scan_msg->ranges[scan_msg->ranges.size() / 2] < 20 && start_velocity > 1) start_velocity -= 1;
 	}
     else {
-		/*if (scan_msg->ranges[scan_msg->ranges.size()/2] > 50) {
+		if (scan_msg->ranges[scan_msg->ranges.size()/2] > 50) {
 	    	is_max_speed_okay_after = true;
             return;
-	}*/
+	}
         int original_max = maximum_element_index(filtered_ranges);
 
         int max_element_index = -1;
