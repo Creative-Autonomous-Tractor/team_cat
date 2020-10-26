aceda6362d7860b18b5c8204e2a8f35fb58f211f
jihoon416 <63445490+jihoon416@users.noreply.github.com>
2020-10-26 20:43:28 +0900

    Forgot jilju_angle is const

--- src/ourdrive_node.cpp
+++ src/ourdrive_node.cpp
@@ -40,7 +40,7 @@ bool is_max_speed_okay = true;
 bool is_max_speed_okay_after = false;
 bool jilju = false;
 
-const double jilju_angle = 0.5 * 3.14 / 180;
+double jilju_angle = 0.5 * 3.14 / 180;
 
 double min(double a, double b) {
     return a > b ? b : a;
