beb22f62a096c478acf358136cfd28cef39063a9
jihoon416 <63445490+jihoon416@users.noreply.github.com>
2020-10-26 19:56:20 +0900

    Edited to not stop at first

--- src/ourdrive_node.cpp
+++ src/ourdrive_node.cpp
@@ -278,6 +278,7 @@ else if (scan_number > scan_number_gijoon){*/
             else if (filtered_ranges[int(filtered_ranges.size() / 2)] < 20 && start_velocity > 1) {
                 start_velocity -= 0.1;
             }
+            else if (filtered_ranges[int(filtered_ranges.size() / 2)] > 20) start_velocity = 20;
             velocity = start_velocity;
             ROS_INFO("Speed is %f and Steering angle is %f", velocity, steering_angle);
         }
