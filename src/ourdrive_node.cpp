8d939d73f2c921a42c619e82327e695f15f5e9e1
jihoon416 <63445490+jihoon416@users.noreply.github.com>
2020-10-27 11:04:55 +0900

    Tolerance needed

--- src/ourdrive_node.cpp
+++ src/ourdrive_node.cpp
@@ -288,7 +288,7 @@ else if (scan_number > scan_number_gijoon){*/
                 ROS_INFO("Started Speed Decrease");
             }
             else if (filtered_ranges[int(filtered_ranges.size() / 2)] > 20) {
-                if (start_velocity > 10) start_velocity = 20;
+                if (start_velocity > 9.9) start_velocity = 20;
                 else start_velocity = 10;
             }
             velocity = start_velocity;
