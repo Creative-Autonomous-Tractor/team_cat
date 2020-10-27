40dcecadef07ebbd3ff3cc85ad502ee3e0f0b0cd
jihoon416 <63445490+jihoon416@users.noreply.github.com>
2020-10-27 11:01:44 +0900

    Sequential Speed Increase

--- src/ourdrive_node.cpp
+++ src/ourdrive_node.cpp
@@ -254,7 +254,7 @@ else if (scan_number > scan_number_gijoon){*/
             //ROS_INFO("Start Velocity is %i or %f", start_velocity, start_velocity);
             if (scan_msg->ranges[scan_msg->ranges.size() / 2] < 5) {
                 is_max_speed_okay = false;
-                start_velocity = 30;
+                start_velocity = 10;
             }
             // if (start_velocity > 1) start_velocity -= 0.01;
             // else if (scan_msg->ranges[scan_msg->ranges.size() / 2] < 15) velocity /= 5; //velocity is 20
@@ -274,7 +274,7 @@ else if (scan_number > scan_number_gijoon){*/
 
             if (scan_msg->ranges[scan_msg->ranges.size() / 2] < 5) {// && filtered_ranges[max_index] < 20) {
                 is_max_speed_okay_after = false;
-                start_velocity = 30;
+                start_velocity = 10;
                 //jilju_angle = 0.8 * 3.14 / 180;
                 //for (int i = 0; i < 1000; i++) ROS_INFO("Jilju End");
                 return;
@@ -287,7 +287,10 @@ else if (scan_number > scan_number_gijoon){*/
                 //jilju_angle = 0.1 * 3.14 / 180;
                 ROS_INFO("Started Speed Decrease");
             }
-            else if (filtered_ranges[int(filtered_ranges.size() / 2)] > 20) start_velocity = 30;
+            else if (filtered_ranges[int(filtered_ranges.size() / 2)] > 20) {
+                if (start_velocity > 10) start_velocity = 20;
+                else start_velocity = 10;
+            }
             velocity = start_velocity;
             //ROS_INFO("Speed is %f and Steering angle is %f", velocity, steering_angle);
         }
